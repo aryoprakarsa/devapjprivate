02b76f7fa8a7ef72de67f927d5c292b32c27e414
Aryo Prakarsa <fojameru@gmail.com>
2020-11-29 23:58:16 +0700

    update

---
+++ assets/js/misc.js
@@ -0,0 +1,40 @@
+(function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function() {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-106136475-1', 'auto');
+ga('send', 'pageview');
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-121983748-1');
+
+  var trackOutboundLink = function(url) {
+     ga('send', 'event', 'outbound', 'click', url, {
+       'transport': 'beacon',
+       'hitCallback': function(){document.location = url;}
+     });
+  }
+
+function KKURDAgLTJJlXMB() {
+    /* Munged addy code - YOU CAN EDIT COMMENT THIS TAG BETWEEN THE TWO STARS */
+    var ynMONwsPMoKAufU = ["109", "101", "x40", "97", "x72", "121", "x6f", "46", "106", "x70"];
+    var AxlyHdhevmHjiiU = ['?', 's', 'u', 'b', 'j', 'e', 'c', 't', '=', 'F', 'r', 'o', 'm', ' ', 'W', 'e', 'b', ' ', 'A', 'r', 'y', 'o', '&', 'c', 'c', '=', '&', 'b', 'c', 'c', '=', '&', 'b', 'o', 'd', 'y', '='];
+    var RQjeNPtZmVFXlPI = ["x6d", "x65", "64", "97", "x72", "x79", "x6f", "x2e", "106", "x70"];
+    document.write("<a href=\"&#x6d;&#00097;&#105;&#x6c;&#x74;&#x6f;&#x3a;");
+    for (i = 0; i < ynMONwsPMoKAufU.length; i++) document.write('&#' + ynMONwsPMoKAufU[i] + ';');
+    for (i = 0; i < AxlyHdhevmHjiiU.length; i++) document.write(AxlyHdhevmHjiiU[i]);
+    document.write('" style="" class="" id="">');
+    for (i = 0; i < RQjeNPtZmVFXlPI.length; i++) document.write('&#' + RQjeNPtZmVFXlPI[i] + ';');
+    document.write('</a>');
+    /* Munged addy code - YOU CAN EDIT COMMENT THIS TAG BETWEEN THE TWO STARS */
+}
